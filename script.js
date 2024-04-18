ecb2b3e8ecd9033fe2a7b28e2e825b20703e736e
Dupsogod <dupsogod@gmail.com>
2024-04-18 22:08:43 +0300

    Add JavaScript for the snake game

---
+++ script.js
@@ -0,0 +1,16 @@
+var canvas = document.getElementById('game-area');
+var ctx = canvas.get2DContext('2d');
+ctx.fillStyle = '#FFF';
+ctx.fillRect(0, 0, 300, 300);
+var snakeX = 0;
+var snakeY = 0;
+var unit_size = 20;
+var snake = [];
+var direction = 'r';
+const updateGameArea = function() {
+    ctx.clearRect(0, 0, 300, 300, '#000');
+    for (var i = 0 i < snake.length; i++) {
+        ctx.fillRect(snake[i].x, snake[i].y, unit_size, unit_size);
+    }
+};
+setSimeInterval(updateGameArea, 200);
